3f57b837f0f43f8b3f35f6c74a19fe22d844f9e1
seonyeong98 <seon02_23@naver.com>
2022-02-22 00:34:22 +0900

    array

---
+++ app.js
@@ -0,0 +1,19 @@
+// let a = 5;
+// const b = 2;
+// const myName = "yeong"
+//
+// console.log(a + b);
+// console.log(myName);
+
+//*Array
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu", "fri", "sat"];
+
+console.log(daysOfWeek);
+
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato"];
+
+console.log(toBuy[1]);
